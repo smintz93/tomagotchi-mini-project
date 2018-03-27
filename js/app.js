f5785ec08ab076869bcb5370f8911da69f6fa5c5
Sam <sammintz1993@gmail.com>
2018-03-27 15:13:40 -0500

    Added Picture and Buttons

---
+++ js/app.js
@@ -0,0 +1,27 @@
+console.log("Linked")
+
+const hunger;
+
+
+
+class Tomogatochi {
+	constructor(name, hunger, sleepiness, boredom, age) {
+		this.name = name;
+		this.hunger = hunger;
+		this.sleepiness = sleepiness;
+		this.boredom = boredom;
+		this.age = age;
+
+	}
+	
+
+
+};
+
+const tomogatochiOne = new Tomogatochi("Sam")
+
+
+
+
+
+
