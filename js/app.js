1013eed4a6a5b3f7c220ed8c4353f189d7803115
Sam <sammintz1993@gmail.com>
2018-03-27 16:50:03 -0500

    Added countdown Timer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 console.log("Linked")
 
-const hunger;
+let time = 15;
 
 
 
@@ -18,10 +18,94 @@ class Tomogatochi {
 
 };
 
-const tomogatochiOne = new Tomogatochi("Sam")
+
+const tomogatochiOne = new Tomogatochi("Sam", 10, 10, 10, 0)
+console.log(tomogatochiOne)
+
+const updateList = function(){
+
+	$("#hunger").text(" Hunger:" + tomogatochiOne.hunger);
+	$("#sleepiness").text(" Sleepiness:" + tomogatochiOne.sleepiness);
+	$("#boredom").text(" Boredom:" + tomogatochiOne.boredom);
+	$("#age").text(" Age:" + tomogatochiOne.age);
+
+
+}
 
 
 
+
+$("#feed").on("click", (e) => {
+
+	tomogatochiOne.hunger--
+
+	// setTimer();
+	updateList();
+})
+
+
+
+$("#sleep").on("click", (e) => {
+
+	tomogatochiOne.sleepiness--
+
+	// setTimer();
+	updateList();
+
+})
+
+$("#play").on("click", (e) => {
+
+	tomogatochiOne.boredom--
+	// setTimer();
+	updateList();
+})
+
+const setTimer = function() {
+	
+	const timer = setInterval(() => {
+		time --
+
+		if(time === 10) {
+			tomogatochiOne.hunger++
+			tomogatochiOne.sleepiness++
+			tomogatochiOne.boredom++
+			tomogatochiOne.age++;
+			updateList();
+		} 
+
+		else if(time === 5) {
+			tomogatochiOne.hunger++
+			tomogatochiOne.sleepiness++
+			tomogatochiOne.boredom++
+			tomogatochiOne.age++;
+			updateList();
+		}
+
+		else if(time === 0) {
+			tomogatochiOne.age++;
+			$("#age").text();
+			updateList();
+
+
+
+			clearInterval(timer)
+			time = 15;
+		}
+
+		$("#timer").text("timer: " + time + "s")
+
+
+	}, 1000);
+
+
+
+};
+
+
+
+
+setTimer();
 
 
 
