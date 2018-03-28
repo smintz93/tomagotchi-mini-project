82841e3453b5457f65c30dcce4eda59e16fed1c5
Sam <sammintz1993@gmail.com>
2018-03-28 08:48:41 -0500

    Changed Timer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 console.log("Linked")
 
-let time = 15;
+let time = 50;
+
+// This happens every refresh // 
+
+
+// START OF GAME. TURNING OFF WHILE TESTING
+
+// const enterName = function(name){
+
+// 	const startName = window.prompt("What would you like to name your Dino?")
+
+// 	const newH2 = $("<h2>")
+
+// 	newH2.appendTo("header")
+ 
+// 	newH2.text("Hello " + startName +"!")
+
+// }
+
+// enterName();
+
+
+
+// window.alert("Click 'Ok' to get started")
+
+
 
 
 
@@ -19,9 +44,20 @@ class Tomogatochi {
 };
 
 
-const tomogatochiOne = new Tomogatochi("Sam", 10, 10, 10, 0)
-console.log(tomogatochiOne)
 
+const dino = () => {
+	const img = $("<img>")
+	img.appendTo($("body"))
+	img.attr("src", "https://i.pinimg.com/originals/3f/6d/fb/3f6dfbd77eefe0287e2539f87430fc52.png")
+}
+
+
+
+
+
+const tomogatochiOne = new Tomogatochi("Sam", 7, 7, 7, 0, dino())
+console.log(tomogatochiOne)
+ 
 const updateList = function(){
 
 	$("#hunger").text(" Hunger:" + tomogatochiOne.hunger);
@@ -31,7 +67,6 @@ const updateList = function(){
 
 
 }
-
 
 
 
@@ -61,12 +96,14 @@ $("#play").on("click", (e) => {
 	updateList();
 })
 
+
+
 const setTimer = function() {
 	
 	const timer = setInterval(() => {
 		time --
 
-		if(time === 10) {
+		if(time === 40) {
 			tomogatochiOne.hunger++
 			tomogatochiOne.sleepiness++
 			tomogatochiOne.boredom++
@@ -74,7 +111,20 @@ const setTimer = function() {
 			updateList();
 		} 
 
-		else if(time === 5) {
+		else if(time === 30) {
+			tomogatochiOne.hunger++
+			tomogatochiOne.sleepiness++
+			tomogatochiOne.boredom++
+			tomogatochiOne.age++;
+			updateList();
+
+		}	else if(time === 25) {
+			tomogatochiOne.hunger++
+			tomogatochiOne.sleepiness++
+			tomogatochiOne.boredom++
+			tomogatochiOne.age++;
+			updateList();
+		} else if(time === 15){
 			tomogatochiOne.hunger++
 			tomogatochiOne.sleepiness++
 			tomogatochiOne.boredom++
@@ -90,10 +140,11 @@ const setTimer = function() {
 
 
 			clearInterval(timer)
-			time = 15;
-		}
+			time = 50;
 
-		$("#timer").text("timer: " + time + "s")
+		} 
+
+		$("#timer").text("Timer: " + time + "s")
 
 
 	}, 1000);
@@ -103,9 +154,20 @@ const setTimer = function() {
 };
 
 
-
-
 setTimer();
+
+
+// 
+
+
+// if(tomogatochiOne.hunger === 10 || tomogatochiOne.sleepiness === 10 || tomogatochiOne.boredom === 10) {
+
+// 		console.log("Dino is dead")
+
+// 		// pass in new Dino 
+// }
+
+
 
 
 
